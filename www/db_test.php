1ccb4175feb2c505f76b6aec5eca62f9dbb22822
komBAS <37916435+kombasltd@users.noreply.github.com>
2018-04-03 13:41:06 +0200

    Generic link to MySQL database
    Add generic link to MySQL database

--- www/db_test.php
+++ www/db_test.php
@@ -1,5 +1,5 @@
 <?php
-/*$mysqli = new mysqli('172.18.0.2', 'dev','dev', 'test' ) or die($mysqli->connect_errno);
+/*$mysqli = new mysqli('mysql', 'dev','dev', 'test' ) or die($mysqli->connect_errno);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
